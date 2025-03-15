9affe7070f6873bed03c6be01b27801d8fd62b8a
A. Ivanov <aleksandr.pv.ivanov@gmail.com>
2025-03-15 22:11:50 +0100

    Ops: Moved pproxy remote URI variable to file

--- ops/create-secrets/create-secrets.sh
+++ ops/create-secrets/create-secrets.sh
@@ -3,7 +3,7 @@ kubectl delete secret --ignore-not-found=true mongo-scraper-secret
 kubectl delete secret --ignore-not-found=true mongo-web-secret
 
 kubectl create secret generic pproxy-secret \
-  --from-literal=remote-uri='ss://password@host:port'
+  --from-env-file=pproxy.env
 
 kubectl create secret generic mongo-scraper-secret \
   --from-env-file=mongo-scraper.env
